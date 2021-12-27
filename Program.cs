5978ad7f93afe787be08efe5c94486d54b34ff99
Ehsan <JMSEhsan@gmail.com>
2021-12-27 00:00:13 -0700

    CS_Enum

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApplication
+{
+  enum Level
+  {
+    Low,
+    Medium,
+    High
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Level myVar = Level.Medium;
+      Console.WriteLine(myVar);
+    }
+  }
+}
